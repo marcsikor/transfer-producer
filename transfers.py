469916501c5f51f3b93ae0665485c5f6e097ac6c
marcsikor <s223464@uek.krakow.edu.pl>
2022-02-06 22:31:33 +0100

    minor corrections and comments

--- transfers.py
+++ transfers.py
@@ -5,20 +5,20 @@ class App:
     def __init__(self, root):
         self.root = root
         self.titles = ["Receiver name", "Receiver name cont.", "Receiver account number", "Value","Sender account number", "Sender name", "Sender name cont.", "Title"]
-        self.entries = []
-        self.labels = []
-        self.language_tracker = 0
+        self.entries = [] # list for storing entry objects
+        self.labels = [] # list for storing label objects (for translation)
+        self.language_tracker = True # language flag
 
-        self.root.title("Transfer order creator 1.0")
+        self.root.title("Transfer order creator 1.0") # window title
         self.root.configure(bg='#d198b7')
 
         self.start()
     
     def start(self):
-        self.title_label = tk.Label(self.root, font=('Ubuntu',18,'bold'), text="Transfer order creator\n", bg="#d198b7", fg='#000000')
-        self.title_label.pack()
+        self.title_label = tk.Label(self.root, font=('Ubuntu',18,'bold'), text="Transfer order creator\n", bg="#d198b7", fg='#000000') #top label
+        self.title_label.pack() # .pack() method used for displaying UI elements
 
-        for i in self.titles:
+        for i in self.titles: # setting up the labels and entry bars 
             l = tk.Label(root, font=('Ubuntu Regular',16), text=i, bg='#d198b7', fg='#000000')
             l.pack()
             self.labels.append(l)
@@ -37,8 +37,8 @@ class App:
         activebackground="#779ecb",
         activeforeground="white", 
         relief='flat'
-        )
-        self.run_button.pack(pady = 10) # run button
+        ) # run button
+        self.run_button.pack(pady = 10)
 
         self.language_button = tk.Button(
         self.root, 
@@ -52,7 +52,7 @@ class App:
         activeforeground="white", 
         relief='flat'
         )
-        self.language_button.pack(pady = 10) # language button
+        self.language_button.pack(pady = 10) # translation/language button
 
     def transfer(self):
 
@@ -71,17 +71,19 @@ class App:
 
         self.temp_files_collector() # removing pdflatex's log and aux files
 
-    def temp_files_collector(self):
+    def temp_files_collector(self): # collecting pdflatex's temporary files (tex, log, aux) - optional functionality
     
-        os.system("rm -rf newtransfer.aux newtransfer.log newtransfer.tex") # collecting pdflatex's temporary files (tex, log, aux) - optional functionality
+        os.system("rm -rf newtransfer.aux newtransfer.log newtransfer.tex") 
 
-    def change_language(self):
-        if self.language_tracker % 2 == 0:
+    def change_language(self): # translating method 
+
+        if self.language_tracker:
             self.titles = ["Nazwa odbiorcy","Nazwa odbiorcy cd.","Nr rachunku odbiorcy","Kwota","Nr rachunku zleceniodawcy","Nazwa zleceniodawcy","Nazwa zleceniodawcy cd.","Tytuł"]
             self.root.title("Kreator poleceń przelewu 1.0")
             self.title_label.configure(text = "Kreator polecenia przelewu\n")
             self.language_button.configure(text = "Change language to english")
             self.run_button.config(text='Wykonaj')
+            self.language_tracker = False
 
         else:
             self.titles = ["Receiver name", "Receiver name cont.", "Receiver account number", "Value","Sender account number", "Sender name", "Sender name cont.", "Title"]
@@ -89,13 +91,12 @@ class App:
             self.title_label.configure(text = "Transfer order creator\n")
             self.language_button.configure(text = "Zmień język na polski")
             self.run_button.config(text='Run')
+            self.language_tracker = True
 
         a = 0
-        for i in self.labels:
+        for i in self.labels: # renaming labels
             i.configure(text=self.titles[a])
             a += 1
-        
-        self.language_tracker += 1
 
 root = tk.Tk()
 app = App(root)
