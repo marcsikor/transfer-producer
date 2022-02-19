35f16386bc503e06dfa0cb24eb95b2447fd396f3
marcsikor <s223464@uek.krakow.edu.pl>
2022-02-19 16:30:49 +0100

    currency change with styling and suggestions

--- transfers.py
+++ transfers.py
@@ -1,11 +1,12 @@
 import tkinter as tk
+from tkinter import ttk
 import os
 
 class App:
     def __init__(self, root):
         self.root = root
         self.titles = ["Receiver name", "Receiver name cont.", "Receiver account number", "Value","Sender account number", "Sender name", "Sender name cont.", "Title"]
-        self.additional_titles = ["Stamp, date and signature of the sender", "Stamp", "Fee", "Voucher for the sender's bank", "Voucher for the sender"]
+        self.additional_titles = ["Stamp, date and signature of the sender", "Stamp", "Fee", "Voucher for the sender's bank", "Voucher for the sender", "D", "T"]
         self.entries = [] # list for storing entry objects
         self.labels = [] # list for storing label objects (for translation)
         self.language_tracker = True # language flag
@@ -16,16 +17,29 @@ class App:
         self.start()
 
     def start(self):
+        # maybe a scrollbar in the future?
+        
+        # scroll = tk.Scrollbar(self.root, orient = 'vertical')
+        # scroll.pack(side = 'right', fill = 'y')
+
         self.title_label = tk.Label(self.root, font=('Ubuntu',18,'bold'), text="Transfer order creator\n", bg="#d198b7", fg='#000000') #top label
         self.title_label.pack() # .pack() method used for displaying UI elements
+
+        # migration to ttk for entries in the future?
+
+        # ttk.Style().map('TEntry',
+        # background = [('active','white')]
+        # )
 
         for i in self.titles: # setting up the labels and entry bars 
             l = tk.Label(root, font=('Ubuntu Regular',16), text=i, bg='#d198b7', fg='#000000')
             l.pack()
             self.labels.append(l)
-            e = tk.Entry(root, bg="#E1BFFF", bd=2, highlightcolor='#779ecb', relief='flat')
+            e = tk.Entry(root, bg="#E1BFFF", bd=2, highlightcolor='#779ecb', selectbackground = '#779ecb', selectforeground = "white", relief='flat')
             e.pack(ipadx = 100)
             self.entries.append(e)
+
+        self.currency_dropdown()        
 
         self.run_button = tk.Button(
         self.root, 
@@ -55,6 +69,35 @@ class App:
         )
         self.language_button.pack(pady = 10) # translation/language button
 
+
+    def currency_dropdown(self):
+
+        # making a label for a dropdown
+
+        l = tk.Label(root, font=('Ubuntu Regular',16), text="Currency", bg='#d198b7', fg='#000000')
+        l.pack()
+        self.labels.append(l)
+
+        # styling (ttk is a pain)
+
+        ttk.Style().configure('TCombobox', fieldbackground = "#E1BFFF", background = '#86C5DA', selectbackground = '#779ecb', selectforeground = "white", relief='flat', borderwidth = "0")
+        ttk.Style().map('TCombobox',
+        background = [('active','#779ecb'), ('pressed', '#779ecb')],
+        relief = [('active', 'flat'),('pressed', 'flat')],
+        arrowcolor = [('active','white'), ('pressed','white')]
+        )
+        
+        self.root.option_add('*TCombobox*Listbox*Background', '#86C5DA')
+        self.root.option_add('*TCombobox*Listbox*selectBackground', '#779ecb')
+        self.root.option_add('*TCombobox*Listbox*selectForeground', 'white')
+
+        # dropdown menu
+
+        self.combo = ttk.Combobox(self.root)
+        self.combo["values"] = ['PLN','USD','GBP']
+        self.combo.current(0)
+        self.combo.pack()
+
     def transfer(self):
 
         with open('transfer.tex') as g: # opening template tex file
@@ -64,8 +107,12 @@ class App:
             a = a.replace('placeholder'+str(i), self.entries[i].get()) # creating new tex code while editing the template on "placeholder*" flag
             a = a.replace('new*placeh'+str(i), self.titles[i]) # renaming the boxes names with "new*placeh" flag
         
-        for i in range(5):
+        for i in range(7):
             a = a.replace('additional*plch'+str(i), self.additional_titles[i]) # changing the constant boxes names
+        
+        for i in range(2):
+            a = a.replace('currplaceh'+str(i), self.combo.get().upper() ) # changing the currency
+
         with open('newtransfer.tex', 'w') as f: # creating new tex file
             f.write(a)
 
@@ -82,8 +129,8 @@ class App:
     def change_language(self): # translating method 
 
         if self.language_tracker:
-            self.titles = ["Nazwa odbiorcy","Nazwa odbiorcy cd.","Nr rachunku odbiorcy","Kwota","Nr rachunku zleceniodawcy","Nazwa zleceniodawcy","Nazwa zleceniodawcy cd.","Tytuł"]
-            self.additional_titles = ["Pieczęć, data i podpis zleceniodawcy", "Pieczęć", "Opłata", "Odcinek dla banku zleceniodawcy", "Odcinek dla zleceniodawcy"]
+            self.titles = ["Nazwa odbiorcy","Nazwa odbiorcy cd.","Nr rachunku odbiorcy","Kwota","Nr rachunku zleceniodawcy","Nazwa zleceniodawcy","Nazwa zleceniodawcy cd.","Tytuł", "Waluta"]
+            self.additional_titles = ["Pieczęć, data i podpis zleceniodawcy", "Pieczęć", "Opłata", "Odcinek dla banku zleceniodawcy", "Odcinek dla zleceniodawcy", "W", "P"]
             self.root.title("Kreator poleceń przelewu 1.0")
             self.title_label.configure(text = "Kreator polecenia przelewu\n")
             self.language_button.configure(text = "Change language to english")
@@ -91,8 +138,8 @@ class App:
             self.language_tracker = False
 
         else:
-            self.titles = ["Receiver name", "Receiver name cont.", "Receiver account number", "Value","Sender account number", "Sender name", "Sender name cont.", "Title"]
-            self.additional_titles = ["Stamp, date and signature of the sender", "Stamp", "Fee", "Voucher for the sender's bank", "Voucher for the sender"]
+            self.titles = ["Receiver name", "Receiver name cont.", "Receiver account number", "Value","Sender account number", "Sender name", "Sender name cont.", "Title", "Currency"]
+            self.additional_titles = ["Stamp, date and signature of the sender", "Stamp", "Fee", "Voucher for the sender's bank", "Voucher for the sender", "D", "T"]
             self.root.title("Transfer order creator 1.0")
             self.title_label.configure(text = "Transfer order creator\n")
             self.language_button.configure(text = "Zmień język na polski")
@@ -103,6 +150,8 @@ class App:
         for i in self.labels: # renaming labels
             i.configure(text=self.titles[a])
             a += 1
+
+        
 
 root = tk.Tk()
 app = App(root)
